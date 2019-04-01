729125bdf6579ef10ba63d0303883ad3609b0bd4
insecurity22 <insecurity22@naver.com>
2019-04-02 04:47:44 +0900

    Create pycharm.sh

---
+++ install-in-ubuntu/pycharm.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+# Install Pycharm
+
+echo -e "\n\nInstall Pycharm\n\n"
+sudo add-apt-repository ppa:mystic-mirage/pycharm
+sudo apt-get update
+sudo apt-get install pycharm-community
+
+# start pycharm
+# pycharm-community
+# 
+#
+# accept
+# http://jalbum.com/board_fasH18/38263
