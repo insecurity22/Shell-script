cd429620e688f0323767a60c583c8d9c9ece4785
insecurity22 <insecurity22@naver.com>
2019-04-02 02:47:31 +0900

    Create wireshark.sh

---
+++ install/wireshark.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+# Install wireshark
+
+function apterror() {
+	if [ "$ERR_MSG" != "" ]; then
+		sudo rm /var/lib/dpkg/lock-frontend
+		sudo rm /var/lib/dpkg/lock
+	fi
+}
+
+sudo apt-get install wireshark
+apterror
