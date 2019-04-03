7c878ec38b4d334ae53ca3774c641778fbf18523
insecurity22 <insecurity22@naver.com>
2019-04-04 01:32:26 +0900

    Update pycharm.sh

--- install-in-ubuntu/pycharm.sh
+++ install-in-ubuntu/pycharm.sh
@@ -6,8 +6,8 @@ sudo add-apt-repository ppa:mystic-mirage/pycharm
 sudo apt-get update
 sudo apt-get install pycharm-community
 
-# start pycharm
-# pycharm-community
+echo -e "\n\nStart Pycharm\n\n"
+pycharm-community
 # 
 #
 # Terminal
