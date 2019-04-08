a4e742f4b6a27599ae145ba986e6075dbdea8f48
insecurity22 <insecurity22@naver.com>
2019-04-08 22:37:29 +0900

    Update sqlmap.sh

--- install-in-ubuntu/sqlmap.sh
+++ install-in-ubuntu/sqlmap.sh
@@ -5,3 +5,12 @@
 sudo apt-get install sqlmap
 sqlmap -h
 sqlmap -hh
+
+# sudo apt-get remove sqlmap
+# sudo apt-get remove --auto-remove sqlmap
+
+# Purged config/data can not be restored by reinstalling the package.
+# sudo apt-get purge --auto-remove sqlmap
+
+# Remove reference
+# http://installion.co.uk/ubuntu/vivid/universe/s/sqlmap/uninstall/index.html
