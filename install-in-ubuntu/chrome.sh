7758372fc90da3699ce8febab31e843f753e1714
insecurity22 <insecurity22@naver.com>
2019-05-13 13:17:20 +0900

    Create chrome.sh

---
+++ install-in-ubuntu/chrome.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+sudo apt-get install libxss1 libgconf2-4 libappindicator1 libindicator7
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+# http://snowdeer.github.io/linux/2018/02/02/ubuntu-16p04-install-chrome/
