f184b9fc785702a45af6670b43457c5a50e2ed5c
insecurity22 <insecurity22@naver.com>
2019-09-15 15:20:18 +0900

    Update express_react.sh

--- Webapp/express_react.sh
+++ Webapp/express_react.sh
@@ -7,39 +7,49 @@ echo -e "\n\n1. Create our express application for the backend.\n"
 echo -e "--> Install express.\n"
 sudo npm install -g express-generator
 
-echo -e "--> Create project\n"
-echo -e "Please select jade or ejs template : "
+echo -e "\n--> Create project\n"
+echo "Please enter jade or ejs template : "
 read template
-if [ ${template} == 'jade']
+if [ ${template} == 'jade' ]
 then
     express react-backend
-elif [ ${template} == 'ejs']
+elif [ ${template} == 'ejs' ]
 then
     express --ejs react-backend
+else
+    echo -e "Please enter again"
+    exit
 fi
 
-echo -e "--> Install related express file\n"
+echo -e "\n--> Install related express file\n"
 cd react-backend
 npm install
 # -----------------------------------------------
 
-echo -e "2. Please press enter after change \"port number\" in \"./bin/www\"\n"
-echo -e "  Because of the crash.\n"
-echo -e ": express 3000 port\n"
-echo -e ": create-react-app 3000 port\n"
+echo -e "2. Please press enter after change \"port number\" in \"./bin/www\""
+echo -e "  Because of the crash."
+echo -e ": express 3000 port"
+echo -e ": create-react-app 3000 port"
 echo -e "So we have to change express port. ex) 3000 -> 3001\n"
+echo -e "If you change port number, Please enter \"yes\""
+read yes
+if [ ${yes} == 'y' || ${yes} == 'yes' ]
+then
+    # -----------------------------------------------
+    echo -e "3. Install \"create-react-app\" module."
+    npm install -g create-react-app
 
-# -----------------------------------------------
-echo -e "3. Install \"create-react-app\" module."
-npm install -g create-react-app
+    echo -e "\n--> create react app\n"
+    create-react-app frontend # create the React app inside the react-backend folder
 
-echo -e "--> create react app\n"
-create-react-app frontend # create the React app inside the react-backend folder
+    # -----------------------------------------------
 
-# -----------------------------------------------
-
-echo -e "express.js project start"
-npm start
+    echo -e "express.js project start"
+    npm start
+else
+    echo -e "Please enter again"
+    exit
+fi    
 
 # Check page
 # localhost:3000
