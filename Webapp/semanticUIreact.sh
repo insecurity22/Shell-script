91416d828894679ceca4405e3fea1981acf1700f
insecurity22 <insecurity22@naver.com>
2019-09-15 21:29:55 +0900

    Create semanticUIreact.sh

---
+++ Webapp/semanticUIreact.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+echo -e "Install semantic ui react"
+npm install semantic-ui-react
+
+npm install semantic-ui-css
+# After install, import the minified CSS file in your app's entry file.
+# import 'semantic-ui-css/semantic.min.css'
+
+
+# https://semantic-ui.com/introduction/getting-started.html
+# https://react.semantic-ui.com/usage
+# semantic-ui and semantic-ui react are different
