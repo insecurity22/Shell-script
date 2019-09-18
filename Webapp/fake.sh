e42d34b1f58adb38690c7b71b1a87132408432f5
insecurity22 <insecurity22@naver.com>
2019-09-19 02:42:38 +0900

    Update fake.sh

--- Webapp/fake.sh
+++ Webapp/fake.sh
@@ -4,3 +4,9 @@
 echo -e "\n\nInstall Atom\n"
 npm install fake --save
 
+
+# usage
+# 1. example : http://zetcode.com/javascript/fakerjs/
+# 2. API method : https://github.com/Marak/faker.js/blob/master/Readme.md
+#                  ex) faker.address.zipCode()
+
