01901df423909870266b5ccd08cbbe44a169ff7b
insecurity22 <insecurity22@naver.com>
2019-09-30 23:44:37 +0900

    Update atom.sh

--- Webapp/atom.sh
+++ Webapp/atom.sh
@@ -5,3 +5,6 @@ echo -e "\n\nInstall Atom\n"
 sudo add-apt-repository ppa:webupd8team/atom
 sudo apt-get update
 sudo apt-get install atom
+
+# Error Xlib: extension "XInputExtension" missing on display ":1".
+# https://www.oipapio.com/question-40345
