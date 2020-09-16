d7c8e7c3e25ad2e235351e19c41cb07d3dbb97a8
insecurity22 <insecurity22@naver.com>
2020-09-17 04:09:39 +0900

    Update express.sh

--- Webapp/express.sh
+++ Webapp/express.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 # After installing nodejs, please install express.
 
+cd react_frontend
+
 # EXPRESS
 echo -e "1. Install express-generator module."
 sudo npm install -g express-generator
@@ -26,7 +28,7 @@ npm install
 echo -e "\n4. Please press after change \"Port Number" in \"./bin/www\" because of the crash."
 echo -e ": Express 3000 port"
 echo -e ": React 3000 port"
-echo -e "So you have to change express port. ex) exfpress 3000 -> 3001\n"
+echo -e "So you have to change express port. ex) express 3000 -> 3001\n"
 
 echo -e ">> If you change port number, please enter \"yes\""
 read yes
@@ -34,3 +36,30 @@ if [ ${yes} == 'y' -o ${yes} == 'yes' ]
 then
     echo -e "express.js start"
     npm start
+
+# 2. Proxy setting
+#
+# /frontend/package.json
+#   "scripts": {
+#       "start": "craco start",
+#       "build": "craco build",
+#       "test": "craco test",
+#       "eject": "craco eject"
+#   },
+#   "proxy": "http://localhost:3001", <----
+
+# 3. Check page
+# 1)
+#   cd express_backend
+#   npm start
+#   localhost:3000
+
+# 2)
+# cd frontend
+# npm start
+# localhost:3001
+
+# ↓
+# https://daveceddia.com/create-react-app-express-backend/
+# https://blueshw.github.io/2017/06/20/create-react-app/
+# https://blog.cloudboost.io/react-express-the-nodejs-way-of-reacting-and-expressing-7a518e4da3
